65100e520dbf1d1eabc0adfdee37d0af6cb1079d
spderman3333 <stefan.v.dikov@gmail.com>
2022-12-25 11:41:24 -0800

    Added more aliens.

--- Alien_invasion/alien_invasion.py
+++ Alien_invasion/alien_invasion.py
@@ -83,8 +83,19 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_alien_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_alien_x):
+            # Create an alien and place it in the row.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
