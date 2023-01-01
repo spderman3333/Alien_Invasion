bb6de8b921cb9a9dde3fb42b2089253469a122cf
spderman3333 <stefan.v.dikov@gmail.com>
2023-01-01 12:53:39 -0800

    Added score, high score, etc

--- Alien_invasion/game_stats.py
+++ Alien_invasion/game_stats.py
@@ -8,8 +8,12 @@ class GameStats:
 
         # Start Alien Invaison in an inactive state.
         self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
     
     def reset_stats(self):
         """Initalize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
